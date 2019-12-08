83b72760055097f0ede8287d9b14309bbd648636
Артем Мамедов <artem.mamedov@gmail.com>
2019-12-08 17:32:59 +0300

    Adde

---
+++ CafeManager/Core/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Core
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public int ID { get; set; }
+        public int Bonuses { get; set; }
+    }
+}
+
